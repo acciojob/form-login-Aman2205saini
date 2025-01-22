0d41219b93d1294774138674e37b1673ea4496ed
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-22 14:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var fname=document.getElementsByName("fname")[0].value
+    var lname=document.getElementsByName("lname")[0].value
+	var name=`${fname} ${lname}`
+	alert(name)
 
 }
